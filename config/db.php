695322da01bd7356b25b29cbf13dfaf2c63e6678
Tannu <ytannu1410@gmail.com>
2026-07-09 13:07:22 +0530

    add environment configuration support

--- config/db.php
+++ config/db.php
@@ -1,10 +1,22 @@
 <?php
 
+// Load .env variables securely if the file exists locally
+$envPath = dirname(__DIR__) . '/.env';
+if (file_exists($envPath)) {
+    $envVars = parse_ini_file($envPath);
+    if ($envVars) {
+        foreach ($envVars as $key => $value) {
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
+        }
+    }
+}
+
 return [
-    'host' => '127.0.0.1',
-    'port' => '3306',
-    'dbname' => 'pavitra_b2b',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4'
+    'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
+    'port' => $_ENV['DB_PORT'] ?? '3306',
+    'dbname' => $_ENV['DB_NAME'] ?? 'pavitra_b2b',
+    'username' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASS'] ?? '',
+    'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4'
 ];
