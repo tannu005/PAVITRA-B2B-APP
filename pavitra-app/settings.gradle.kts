73c8c7c20446d49dc5f4aa3e5a8a0c74515b1ba8
Tannu <ytannu1410@gmail.com>
2026-07-13 16:12:11 +0530

    update gradle build

--- pavitra-app/settings.gradle.kts
+++ pavitra-app/settings.gradle.kts
@@ -24,10 +24,8 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
-
 rootProject.name = "PavitraB2B"
 include(":app")
