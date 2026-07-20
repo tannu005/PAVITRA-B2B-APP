f23b8307afebca70df71d33acb67d4cbd089ae2e
Tannu <ytannu1410@gmail.com>
2026-07-20 13:22:24 +0530

    fix debug script

--- public/debug_db.php
+++ public/debug_db.php
@@ -1,5 +1,10 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+spl_autoload_register(function ($class) {
+    $file = dirname(__DIR__) . '/src/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 $app = new \Core\Application();
 $db = $app->db;
 $products = $db->query("SELECT p.title, v.image_url FROM products p LEFT JOIN product_variants v ON p.id = v.product_id LIMIT 10")->fetchAll();
